73e07d5c678674d53245d987daf36d3a6b593953
hajeongheon <wjdgjs2476@naver.com>
2022-07-06 09:03:49 +0900

    first commit

---
+++ ch6/nested_loop_file/nested_loop.c
@@ -0,0 +1,53 @@
+/* 중첩된 루프 */
+/* <ex1> */
+/*
+// 원하는 알파벳 부터 원하는 알파벳 까지 원하는 갯수만큼 출력
+#include <stdio.h>
+
+int main()
+{
+    const int num = 5;
+    const int first = 'A';
+    const int last = 'K';
+
+    int r;
+    int c;
+
+    for (r = 0; r < num; ++r)   // low loop
+    {
+        for (c = first; c <= last; c++)
+        {
+            printf("%c ", c);
+        }
+        printf("\n");
+    } 
+
+    return 0;
+}
+*/
+
+/* <ex2> */
+// 삼각형 형태로 만들어보기
+
+#include <stdio.h>
+
+int main()
+{
+    const int num = 10;
+    const int first = 'A';
+    const int last = 'K';
+
+    int c = 0;
+    int r = 0;
+    
+    for (r = 0; r <= num; ++r)
+    {
+        for (c = first; c <= first + r; ++c)
+        {
+            printf("%c ", c);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
