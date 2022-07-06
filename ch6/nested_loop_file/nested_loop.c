f51537747c42db1fe7657b9e520d2b0ee3b55d83
hajeongheon <wjdgjs2476@naver.com>
2022-07-06 14:24:08 +0900

    Add example 3 to nested_loop.c

--- ch6/nested_loop_file/nested_loop.c
+++ ch6/nested_loop_file/nested_loop.c
@@ -27,6 +27,7 @@ int main()
 */
 
 /* <ex2> */
+/*
 // 삼각형 형태로 만들어보기
 
 #include <stdio.h>
@@ -46,6 +47,34 @@ int main()
         {
             printf("%c ", c);
         }
+        printf("\n");
+    }
+
+    return 0;
+}
+*/
+
+/* <ex3> */
+// 역삼각형으로 만들기 
+
+#include <stdio.h>
+
+int main()
+{
+    int const last = 'L';
+    int const count = 12;
+    
+    int first = 'A';
+    int c;
+    int r;
+
+    for (r = 0; r <= count; ++r)
+    {
+        for (c = first; c <= last; ++c)
+        {
+            printf("%c", c);
+        }
+        first += 1;
         printf("\n");
     }
 
