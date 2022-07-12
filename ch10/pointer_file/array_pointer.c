fb41aeb95cdd44f08ffda8d53171f7b52d585c2c
hajeongheon <wjdgjs2476@naver.com>
2022-07-12 11:48:46 +0900

    first commit

---
+++ ch10/pointer_file/array_pointer.c
@@ -0,0 +1,36 @@
+/* 배열을 이용한 포인터 */
+// 포인터가 배열을 대체할 수 있다.
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[10];
+
+    int const num = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < num; ++i)
+        arr[i] = (i + 1) * 100;
+
+    // int* ptr = arr; // 배열의 첫 주소를 ptr에 대입
+
+    // printf("%p %p %p\n", ptr, arr, &arr[0]);    // 0x16cee34e0 0x16cee34e0 0x16cee34e0
+    // // %p : 입력을 포인터 (주소)로 해석(16진수)
+    // ptr += 2;
+
+    // printf("%p %p %p\n", ptr, arr + 2, &arr[2]);    // 0x16dcf34c8 0x16dcf34c8 0x16dcf34c8
+
+    // // arr += 2;    // pointer끼리 산술연산하는 것은 가능하지만 배열은 안된다. 배열은 대표 주소이므로 주소가 바뀌면 안된다.
+    
+    // printf("%d %d %d\n", *(ptr + 1), *(arr + 3), arr[3]);   // 400 400 400
+
+    /* <ex> 일반적인 pointer 사용 */
+
+    int *ptr = arr;
+    for (int i = 0; i < num; ++i)
+    {
+        printf("%d %d\n", *ptr++, arr[i]);  // ptr이 하나씩 증가하면서 출력, 100 200 300 400 이런식의 출력
+        printf("%d %d\n", *(ptr + i), arr[i]);  // 첫 주소 ptr에서 i만큼 건너뛰어서 출력 (위의 식과 동일)
+        printf("%d %d\n", *ptr + i, arr[i]);    // 100 + 1, 100 + 2 이런식의 출력
+    }
+    return 0;
+}
