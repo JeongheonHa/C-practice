e36456b50298b9db20c9aed46c7054847d19c76a
hajeongheon <wjdgjs2476@naver.com>
2022-07-14 23:53:13 +0900

    modify remark

--- ch10/dimension_file/multiD_array_founction.c
+++ ch10/dimension_file/multiD_array_founction.c
@@ -1,11 +1,11 @@
 /* 다차원 배열을 함수에게 전달해주는 방법 */
-
+// 다차원 배열에서 배열을 매개변수로 받을 경우 배열의 첫번째 주소(포인터)를 받기 때문에 포인터 연산의 단위를 함수가 알기위해서 cols는 생략할 수 없지만 rows는 생략할 수 있다.
 #include <stdio.h>
 
 #define ROWS 3
 #define COLS 4
 
-int sum2d_1(int ar[ROWS][COLS]);
+int sum2d_1(int ar[ROWS][COLS]);  
 int sum2d_2(int ar[][COLS], int row);   // 다차원의 경우 첫번째 []만 생략 가능, []가 생략가능한 이유 : 배열의 이름은 첫 원소의 주소이기 때문에 포인터로 변환된다.
 // int sum2d_2(int [][CLOS], int row);  // 이름 생략 가능
 // int sum2d_2(int (*ar)[COLS], int row)    // COLS 개수만큼의 원소를 갖는 배열의 포인터 하나 
@@ -28,30 +28,30 @@ int main()
 
     printf("Sim of all elements = %d\n", sum2d_1(data));
     printf("Sum of all elements = %d\n", sum2d_2(data, ROWS));
-    printf("Sum of all elements = %d\n", sum2d_3(data[0][0], ROWS, COLS));
+    printf("Sum of all elements = %d\n", sum2d_3(&data[0][0], ROWS, COLS));
 
     /* Variable - Length Arrays (VLAs : 가변 길이 배열) */
     // 변수로 길이를 정할 수 있는 배열
 
-    int n;
+    // int n;
 
-    printf("Input array length : ");
-    scanf("%d", &n);
+    // printf("Input array length : ");
+    // scanf("%d", &n);
     
-    float my_arr[n];    // 길이 선언후 길이를 바꿀 수 없다.
+    // float my_arr[n];    // 길이 선언후 길이를 바꿀 수 없다.
 
-    for (int i = 0; i < n; ++i)
-        my_arr[i] = (float)i;
+    // for (int i = 0; i < n; ++i)
+    //     my_arr[i] = (float)i;
         
-    for (int i = 0; i < n; ++i)
-        printf("%f\n", my_arr[i]);
+    // for (int i = 0; i < n; ++i)
+    //     printf("%f\n", my_arr[i]);
 
-    int data[ROWS][COLS] = {
-                                {1, 2, 3, 4},
-                                {5, 6, 7, 8},
-                                {9, 0, 1, 2}
-                                                };
-    printf("%d\n", sum2d_4(ROWS, COLS, data));
+    // int data[ROWS][COLS] = {
+    //                             {1, 2, 3, 4},
+    //                             {5, 6, 7, 8},
+    //                             {9, 0, 1, 2}
+    //                                             };
+    // printf("%d\n", sum2d_4(ROWS, COLS, data));
 
     return 0;
 }
@@ -74,7 +74,7 @@ int sum2d_2(int ar[][COLS], int row)
     return tot;
 }
 
-int sum2d_3(int* ar, int row, int col)
+int sum2d_3(int* ar, int row, int col)  // 포인터를 이용할 경우 다차원을 1차원 배열로 사용할 수 있다.
 {
     int r, c, tot = 0;
     for (r = 0; r < ROWS; r++)
@@ -83,7 +83,7 @@ int sum2d_3(int* ar, int row, int col)
     return tot;
 }
 
-int sum2d_4(int row, int col, int ar[row][col]) // 매크로를 덜 사용하기 때문에 간결하며 정리가 된 느낌이다, (매개변수 만으로 함수의 내용을 정리)
+int sum2d_4(int row, int col, int ar[row][col]) // 매크로를 덜 사용하기 때문에 간결하며 정리가 된 느낌이다. (매개변수 만으로 함수의 내용을 정리)
 {
     int r, c, tot = 0;
     for (r = 0; r < row; r++)
