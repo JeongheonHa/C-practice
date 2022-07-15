1759ba40b606f11a1ae015bc7060700d8a6808d0
hajeongheon <wjdgjs2476@naver.com>
2022-07-16 01:44:48 +0900

    first commit

---
+++ ch11/selection_sort_file/selection_sort.c
@@ -0,0 +1,51 @@
+/* 선택 정렬 */
+// https://www.geeksforgeeks.org/selection-sort/
+// 오름차순 정렬
+#include <stdio.h>
+
+void swap(int* xp, int* yp);
+void printArray(int arr[], int size);
+void selectionSort(int arr[], int n);
+
+int main()
+{
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    selectionSort(arr, n);
+
+    printArray(arr, n);
+
+    return 0;
+}
+
+void swap(int* xp, int* yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; ++i)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+void selectionSort(int arr[], int n)
+{
+    int min;
+    for (int i = 0; i < n - 1; ++i)
+    {
+        min = i;
+        for (int j = i + 1; j < n; ++j)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        swap(&arr[i], &arr[min]);
+    }
+}
