95837f0831c56395b3d6680c65def00c2909797a
hajeongheon <wjdgjs2476@naver.com>
2022-07-16 14:33:00 +0900

    Add example

--- ch11/selection_sort_file/selection_sort.c
+++ ch11/selection_sort_file/selection_sort.c
@@ -1,51 +1,105 @@
 /* 선택 정렬 */
 // https://www.geeksforgeeks.org/selection-sort/
 // 오름차순 정렬
-#include <stdio.h>
+// #include <stdio.h>
 
-void swap(int* xp, int* yp);
-void printArray(int arr[], int size);
-void selectionSort(int arr[], int n);
+// void swap(int* xp, int* yp);
+// void printArray(int arr[], int size);
+// void selectionSort(int arr[], int n);
+
+// int main()
+// {
+//     int arr[] = {64, 25, 12, 22, 11};
+//     int n = sizeof(arr) / sizeof(arr[0]);
+
+//     selectionSort(arr, n);
+
+//     printArray(arr, n);
+
+//     return 0;
+// }
+
+// void swap(int* xp, int* yp)
+// {
+//     int temp = *xp;
+//     *xp = *yp;
+//     *yp = temp;
+// }
+
+// void printArray(int arr[], int size)
+// {
+//     for (int i = 0; i < size; ++i)
+//         printf("%d ", arr[i]);
+//     printf("\n");
+// }
+
+// void selectionSort(int arr[], int n)
+// {
+//     int min;
+//     for (int i = 0; i < n - 1; ++i)
+//     {
+//         min = i;
+//         for (int j = i + 1; j < n; ++j)
+//         {
+//             if (arr[j] < arr[min])
+//             {
+//                 min = j;
+//             }
+//         }
+//         swap(&arr[i], &arr[min]);
+//     }
+// }
+
+/* 선택정렬을 이용한 문자열 정렬 */
+
+#include <stdio.h>
+#include <string.h>
+
+void swap(char** xp, char** yp);
+void printStringArray(char* arr[], int size);
+void selectionSort(char* arr[], int n);
 
 int main()
 {
-    int arr[] = {64, 25, 12, 22, 11};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    char* arr[] = {"Cherry", "AppleBee", "Pineapple","Apple", "Orange"};
+    int n =  sizeof(arr) / sizeof(arr[0]);
+
+    printStringArray(arr, n);
 
     selectionSort(arr, n);
 
-    printArray(arr, n);
+    printStringArray(arr, n);
 
     return 0;
 }
 
-void swap(int* xp, int* yp)
-{
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-
-void printArray(int arr[], int size)
+void printStringArray(char* arr[], int size)
 {
     for (int i = 0; i < size; ++i)
-        printf("%d ", arr[i]);
-    printf("\n");
+    {
+        printf("%s\n", *(arr + i));
+    }
 }
 
-void selectionSort(int arr[], int n)
+void selectionSort(char* arr[], int n)
 {
-    int min;
-    for (int i = 0; i < n - 1; ++i)
+    int i, j, min_idx;
+
+    for (i = 0; i < n - 1; ++i)
     {
-        min = i;
-        for (int j = i + 1; j < n; ++j)
+        min_idx = i;
+        for (j = i + 1; j < n; ++j)
         {
-            if (arr[j] < arr[min])
-            {
-                min = j;
-            }
+            if (strcmp(arr[j], arr[min_idx]) < 0)
+            min_idx = j;
         }
-        swap(&arr[i], &arr[min]);
+        swap(&arr[min_idx], &arr[i]);
     }
+}
+
+void swap(char** xp, char** yp)
+{
+    char* temp = *xp;
+    *xp = *yp;
+    *yp = temp;
 }
