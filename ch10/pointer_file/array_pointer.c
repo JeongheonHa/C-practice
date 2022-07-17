e21ab0803a72bf07caeda79dc2ab8fbf72d80d9a
hajeongheon <wjdgjs2476@naver.com>
2022-07-17 14:24:58 +0900

    modify remark

--- ch10/pointer_file/array_pointer.c
+++ ch10/pointer_file/array_pointer.c
@@ -15,11 +15,11 @@ int main()
 
     // printf("%p %p %p\n", ptr, arr, &arr[0]);    // 0x16cee34e0 0x16cee34e0 0x16cee34e0
     // // %p : 입력을 포인터 (주소)로 해석(16진수)
-    // ptr += 2;
+    // ptr += 2;    // 배열의 대표주소에서 2 더한 주소를 포인터에 저장
 
     // printf("%p %p %p\n", ptr, arr + 2, &arr[2]);    // 0x16dcf34c8 0x16dcf34c8 0x16dcf34c8
 
-    // // arr += 2;    // pointer끼리 산술연산하는 것은 가능하지만 배열은 안된다. 배열은 대표 주소이므로 주소가 바뀌면 안된다.
+    // // arr += 2;    // pointer끼리 산술연산하는 것은 가능하지만 배열은 안된다. 배열의 대표주소가 바뀌는 것이므로 안된다.
     
     // printf("%d %d %d\n", *(ptr + 1), *(arr + 3), arr[3]);   // 400 400 400
 
@@ -29,6 +29,7 @@ int main()
     for (int i = 0; i < num; ++i)
     {
         printf("%d %d\n", *ptr++, arr[i]);  // ptr이 하나씩 증가하면서 출력, 100 200 300 400 이런식의 출력
+                                            // ptr의 dereferencing한 값을 접근해서 출력 후 후의 연산자로 인해 ptr의 값(주소 arr) 증가
         printf("%d %d\n", *(ptr + i), arr[i]);  // 첫 주소 ptr에서 i만큼 건너뛰어서 출력 (위의 식과 동일)
         printf("%d %d\n", *ptr + i, arr[i]);    // 100 + 1, 100 + 2 이런식의 출력
     }
--- ch10/pointer_file/compatibility.c
+++ ch10/pointer_file/compatibility.c
@@ -11,20 +11,20 @@ int main()
     // double* pd = &x;
     // // pd = p1; // warning 포인터를 복잡하게 만든 코드는 좋은 코드가 아니다.
 
-    // int* pt;
-    // int(*pa)[3];    // 원소가 3개인 배열에 대한 포인터
-    // int ar1[2][3] = {3, };  // 원소가 3개인 배열의 2개의 배열
-    // int ar2[3][2] = {7, };
-    // int** p2;
+    int* pt;
+    int(*pa)[3];    // 원소가 3개인 배열에 대한 포인터 (다차원 배열을 담을때 유용)
+    int ar1[2][3] = {3, };  // 원소가 3개인 배열의 2개의 배열
+    int ar2[3][2] = {7, };
+    int** p2;
 
-    // pt = &ar1[0][0];
-    // pt = ar1[0];
-    // // pt = ar1;    // warning, ar1은 이중 배열이기때문에 그냥 포인터에는 담을 수 없다. 
-    // pa = ar1;   // 원소가 3개인 배열이기 때문에 대입 가능
-    // // pa = ar2;    // warning
-    // p2 = &pt;
-    // *p2 = ar2[0];
-    // // p2 = ar2;    // warning
+    pt = &ar1[0][0];
+    pt = ar1[0];
+    pt = ar1;    // warning, ar1은 이중 배열이기때문에 그냥 포인터에는 담을 수 없다. 
+    pa = ar1;   // 원소가 3개인 배열이기 때문에 대입 가능
+    // pa = ar2;    // warning
+    p2 = &pt;
+    *p2 = ar2[0];
+    // p2 = ar2;    // warning
 
     // // <주의 사항>
     // // p2 : pointer to pointer to int
@@ -49,11 +49,11 @@ int main()
     // pp2 = &p1;  // 고정 시키고 싶다면 const int** const pp2 = &p1; 입력
 
     // <2>  이런 상황을 만들지 않도록 한다.
-    const int** pp2;
-    int* p1;
-    const int n = 13;
-    pp2 = &p1;
-    *pp2 = &n;
-    *p1 = 10;   // visualstudio에서는 const int n = 13;이지만 값이 10으로 바뀐다.
-                // c++ compiler는 warning (vscode는 c++에 좀 더 가깝기 때문에 warning)
+//     const int** pp2;
+//     int* p1;
+//     const int n = 13;
+//     pp2 = &p1;
+//     *pp2 = &n;
+//     *p1 = 10;   // visualstudio에서는 const int n = 13;이지만 값이 10으로 바뀐다.
+//                 // c++ compiler는 warning (vscode는 c++에 좀 더 가깝기 때문에 warning)
 }
