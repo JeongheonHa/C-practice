f939c0538f664d988bd9f02cbc9a425365f7d874
hajeongheon <wjdgjs2476@naver.com>
2022-07-19 19:48:58 +0900

    Add statement

--- ch12/07_memory_leak_file/main.c
+++ ch12/07_memory_leak_file/main.c
@@ -5,11 +5,13 @@
 
 int main()
 {
-    printf("Dummy Output\n");
 
+    printf("Dummy Output\n");
+    int* ptr_backup = NULL;
+    // for(int k = 0; k < 1000000; ++k) // 메모리 할당을 free없이 계속 줄 경우 (메모리 누수, 절대 발생해서는 안된다.)
     {
         int n = 100000000;
-        int* ptr = (int*)malloc(n * sizeof(int));
+        int* ptr = (int*)malloc(n * sizeof(int));   // ptr 변수는 block을 나가면 stack에서 사라진다.
 
         if (!ptr)   // if(ptr == NULL)
         {
@@ -18,5 +20,17 @@ int main()
         }
         for (int i = 0; i < n; ++i)
             ptr[i] = i + 1;
+
+        printf("%d %d\n", ptr[0], ptr[1]);
+
+        ptr_backup = ptr;   // 주소를 옮겨적어 놓았다면 ptr변수가 block을 나가 사라져도 ptr_backup 변수로 다시 사용할 수 있다.
+
+        // free(ptr);
+        // ptr = NULL;
     }
+
+    printf("%d %d\n", ptr_backup[0], ptr_backup[1]);
+    printf("Dummy Output\n");
+
+    return 0;
 }
