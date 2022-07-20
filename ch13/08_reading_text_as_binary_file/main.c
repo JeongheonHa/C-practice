6488a920684fbae707072288796ae46101c6a2fa
hajeongheon <wjdgjs2476@naver.com>
2022-07-20 23:15:28 +0900

    modify remark

--- ch13/08_reading_text_as_binary_file/main.c
+++ ch13/08_reading_text_as_binary_file/main.c
@@ -1,4 +1,4 @@
-/* 테스트 파일 바이너리 처럼 읽기 */
+/* 텍스트 파일 바이너리 처럼 읽기 */
 
 #include <stdio.h>
 
@@ -10,7 +10,7 @@ int main()
 
     while (fread(&ch, sizeof(unsigned char), 1, fp) > 0)    // fread함수의 반환 값은 읽어드린 데이터의 양이다.
     {
-        printf("%hhu %c\n", ch, ch);    // 알파벳이나 숫자를 표현할 때는 1byte를 사용하고 한글을 사용할 때는 3byte씩 묵어서 사용한다는 것을 알 수 있다.
+        printf("%hhu %c\n", ch, ch);    // 알파벳이나 숫자를 표현할 때는 1byte를 사용하고 한글을 사용할 때는 3byte씩 묶어서 사용한다는 것을 알 수 있다.
     }
 
     fclose(fp);
